e0634b79cf2125aedc12e06525bfd7b5b6fd82cc
Godson <nwankwogodsonc@gmail.com>
2022-06-21 20:17:08 +0100

    css framework

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from 'react'
 import './App.css'
 import Books from './views/Books'
-import { ChakraProvider } from '@chakra-ui/react'
+
 
 function App() {
 	return (
-		<ChakraProvider>
 			<div className='App'>
 				<Books />
 			</div>
-		</ChakraProvider>
 	)
 }
 
--- src/views/Books.js
+++ src/views/Books.js
@@ -1,69 +1,72 @@
-import { Container, Grid, GridItem, Text } from '@chakra-ui/react'
-import React, { useEffect, useState } from 'react'
-import { fetchAllBooks } from '../services/book'
+import { Container, Grid } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { fetchAllBooks } from "../services/book";
 
 const Books = () => {
-	const [books, setBooks] = useState([])
-	const [loading, setLoading] = useState(true)
+  const [books, setBooks] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-	useEffect(() => {
-		fetchAllBooks()
-			.then((res) => {
-				setBooks(res.results)
-				setLoading(false)
-			})
-			.catch((err) => {
-				console.log(err)
-			})
-	}, [])
+  useEffect(() => {
+    fetchAllBooks()
+      .then((res) => {
+        setBooks(res.results);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
-	const getBookImage = (resource) => {
-		let obj = resource.find((o) => o.type === 'image/jpeg')
-		console.log(obj)
+  const getBookImage = (resource) => {
+    let obj = resource.find((o) => o.type === "image/jpeg");
+    console.log(obj);
 
-		return ''
-	}
+    return "";
+  };
 
-	return (
-		<>
-			{loading ? (
-				<div>Loading</div>
-			) : (
-				<Container>
-					{books.map((book, index) => {
-						return (
-							<Grid
-								key={index}
-								my={5}
-								templateColumns='repeat(5, 1fr)'
-								gap={3}>
-								{getBookImage(book.resources)}
-								<GridItem colSpan={1} w='100%' h='10'>
-									<img
-										alt={book.title}
-										height={10}
-										width={40}
-										src='https://www.gutenberg.org/cache/epub/84/pg84.cover.small.jpg'
-									/>
-								</GridItem>
-								<GridItem colSpan={3} w='100%' h='10'>
-									<Text fontSize='sm'>{book.title}</Text>
-									<Text fontSize='xs'>
-										by {book.agents[0].person}
-									</Text>
-								</GridItem>
-								<GridItem colSpan={1} w='100%' h='10'>
-									<Text color='blue.400' fontSize='xs'>
-										View
-									</Text>
-								</GridItem>
-							</Grid>
-						)
-					})}
-				</Container>
-			)}
-		</>
-	)
-}
+  return (
+    <>
+      {loading ? (
+        <div>Loading</div>
+      ) : (
+        <Container>
+          {books.map((book, index) => {
+            return (
+              <Grid
+                container
+                spacing={3}
+                item
+                xs="auto"
+                key={index}
+                my={5}
+                templateColumns="repeat(5, 1fr)"
+                gap={3}
+              >
+                {getBookImage(book.resources)}
+                <text item xs={1} colSpan={1} w="100%" h="10">
+                  <img
+                    alt={book.title}
+                    height={80}
+                    width={60}
+                    src="https://www.gutenberg.org/cache/epub/84/pg84.cover.small.jpg"
+                  />
+                </text>
+                <text>
+                  <text fontSize="sm">{book.title}</text>
+                  <text fontSize="xs">by {book.agents[0].person}</text>
+                </text>
+                <text colSpan={5} w="100%" h="10">
+                  <text color="blue.400" fontSize="xm">
+                    View
+                  </text>
+                </text>
+              </Grid>
+            );
+          })}
+        </Container>
+      )}
+    </>
+  );
+};
 
-export default Books
+export default Books;
